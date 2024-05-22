c8ca54fa2dcb13d0d4bb8dadb60e52428c5c1ca1
Abhinav Muraleedharan <abhinavmuraleedharan@gmail.com>
2024-05-22 11:15:33 -0400

    modified linearization code

--- gemma/linearize_gemma.py
+++ gemma/linearize_gemma.py
@@ -1,8 +1,32 @@
 import jax.numpy as jnp
 from jax import linearize
+from gemma import params as params_lib
+import sentencepiece as spm
+from gemma import transformer as transformer_lib
 
-logits, f_jvp = linearize(Gemma, params, x) # call linearize method
+
+CKPT_PATH = " path to model weights"
+TOKENIZER_PATH = "path to tokenizer"
+
+vocab = spm.SentencePieceProcessor()
+vocab.Load(TOKENIZER_PATH)
+
+GEMMA_VARIANT = '2b-it' # @param ['2b', '2b-it'] {type:"string"}
+
+params = params_lib.load_and_format_params(CKPT_PATH)
+
+transformer_config = transformer_lib.TransformerConfig.from_params(
+    params=params,
+    cache_size=1024
+)
+
+transformer = transformer_lib.Transformer(transformer_config)
+
+logits, f_jvp = linearize(transformer, params['transformer']) # call linearize method
+
 gemma_linear = logits + f_jvp(param_diff,x_zeros) # define gemma_linear
+
+
 
 # write code to sample from linear gemma model
 
@@ -12,6 +36,9 @@ gemma_linear = logits + f_jvp(param_diff,x_zeros) # define gemma_linear
     # estimate val loss
 
     # print out a set of outputs and compare both. 
+
+
+
 
 
 
